a3bfc92a7f86f2a04f66bbf168ec32049db6c715
Kirill Belov <kirillbelovtest@gmail.com>
2024-05-02 15:55:43 +0400

    update

--- Program.cs
+++ Program.cs
@@ -2,5 +2,5 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
-app.MapReverseProxy();
+app.MapReverseProxy(c => c.UseHttpLogging());
 app.Run();
